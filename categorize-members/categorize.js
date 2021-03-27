7c0b66fdf6508d5837e8b8fb2c999eafd05fd433
Micah Lund <micah.r.lund@gmail.com>
2021-03-27 14:47:31 -0600

    Added an if condition to return proper category based on member criteria.

--- categorize-members/categorize.js
+++ categorize-members/categorize.js
@@ -1,9 +1,13 @@
-/*
-Input: [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
-> 55 and > 7 = Senior
-Output: ["Open", "Open", "Senior", "Open", "Open", "Senior"]
-*/
+/*Input: [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+    > 55 and > 7 = Senior
+Output: ["Open", "Open", "Senior", "Open", "Open", "Senior"]*/
 
 const openOrSenior = (data) => {
-    //logic to categorize members based on inputed data.
+    data.map((input) => {
+        if(input[0] > 55 && input[1] > 7) {
+            return "Senior"
+        } else {
+            return "Open"
+        }
+    })
 }
