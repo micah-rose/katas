a729ecf7f18c8c5a3ea7996889125f3591b36a9b
Micah Lund <micah.r.lund@gmail.com>
2021-04-03 13:41:56 -0600

    Completed dubstep challenge using REGEX, string.replace, and string.trim to get desired result.

--- dubstep/dubstep.js
+++ dubstep/dubstep.js
@@ -1,3 +1,6 @@
 const songDecoder = (song) => {
-    //logic to decode a song
+    console.log(song.replace(/(WUB)+/g, ' ').trim()); //Uses REGEX to identify the WUB instances and then splits it by a single space
 }
+
+songDecoder('WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB');
+// => WE ARE THE CHAMPIONS MY FRIEND
