a8c188ad823c801a197389b3e372b41349fa06dc
Micah Lund <micah.r.lund@gmail.com>
2021-04-06 16:16:08 -0600

    Added logic to move the string of numbers into an array of integers.

--- high-low/high-low.js
+++ high-low/high-low.js
@@ -1,5 +1,8 @@
 var numbers = "3 6 9 2 5 4 0"
 
-const highLow = (numbers) => {
-    //logic for return a string of numbers from highest to lowest
+const highLow = (stringNumbers) => {
+    var intArray = stringNumbers.split(' ').map( x => +x)
+    console.log(intArray)
 }
+
+highLow(numbers);
