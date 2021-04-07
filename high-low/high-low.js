c47421d3109dc2dd2c8a9145e589bb3a9e102e96
Micah Lund <micah.r.lund@gmail.com>
2021-04-07 16:48:57 -0600

    Cleaned up the code to be more streamlined.

--- high-low/high-low.js
+++ high-low/high-low.js
@@ -1,8 +1,10 @@
 var numbers = "3 6 9 2 5 4 0"
 
 const highLow = (stringNumbers) => {
-    var intArray = stringNumbers.split(' ').map( x => +x)
-    var sortedArray = intArray.sort(function(a, b){return b-a});
+    var sortedArray = stringNumbers.split(' ')
+        .map( x => +x)
+        .sort(function(a, b){return b-a});
+
     let finalNumbers = "";
     sortedArray.forEach(function(e) {finalNumbers += e + " "});
     console.log(finalNumbers)
