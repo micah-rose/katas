ec7640adbc71a4242aa4c60a90058ecf5ee48831
Micah Lund <micah.r.lund@gmail.com>
2021-04-07 16:46:24 -0600

    Completed result of returning numbers as numbers listed from highest to lowest on one line.

--- high-low/high-low.js
+++ high-low/high-low.js
@@ -3,7 +3,9 @@ var numbers = "3 6 9 2 5 4 0"
 const highLow = (stringNumbers) => {
     var intArray = stringNumbers.split(' ').map( x => +x)
     var sortedArray = intArray.sort(function(a, b){return b-a});
-    console.log(sortedArray)
+    let finalNumbers = "";
+    sortedArray.forEach(function(e) {finalNumbers += e + " "});
+    console.log(finalNumbers)
 }
 
 highLow(numbers);
