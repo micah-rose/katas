c89258d13db9ebbd2aa01f690217fc2481e66d17
Micah Lund <micah.r.lund@gmail.com>
2021-04-07 16:17:28 -0600

    Applied a sort method with a compare function to define how the numbers in the array should be sorted.

--- high-low/high-low.js
+++ high-low/high-low.js
@@ -2,7 +2,8 @@ var numbers = "3 6 9 2 5 4 0"
 
 const highLow = (stringNumbers) => {
     var intArray = stringNumbers.split(' ').map( x => +x)
-    console.log(intArray)
+    var sortedArray = intArray.sort(function(a, b){return b-a});
+    console.log(sortedArray)
 }
 
 highLow(numbers);
