5de2b65dbd87f80853768602f72e5c8bcf495f85
Micah Lund <micah.r.lund@gmail.com>
2021-04-12 21:59:53 -0600

    Added logic to sort the numbers from lowest to highest. This will allow me to then focus on logic to identify the two lowest numbers to sum them together.

--- sum-two/sum-two.js
+++ sum-two/sum-two.js
@@ -1,3 +1,13 @@
-const sumTwo = () => {
-    //logic to find the two smallest numbers and return their sum
+//[19, 5, 42, 2, 77]
+//5 + 2 = 7
+
+const numbers = [19, 5, 42, 2, 77]
+
+const sumTwo = (numbers) => {
+    var sortedNumbers = numbers.map( x => +x)
+        .sort(function(a, b){return a-b});
+
+    console.log(sortedNumbers)
 }
+
+sumTwo(numbers);
