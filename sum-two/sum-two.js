fd58a6eb31eb6c193400fb00ce9f8d267ce5000e
Micah Lund <micah.r.lund@gmail.com>
2021-04-13 22:31:35 -0600

    Refactored the code to be more clean and simple. Used some array destructuring and removed the mapping since it wasn't needed.

--- sum-two/sum-two.js
+++ sum-two/sum-two.js
@@ -4,11 +4,8 @@
 const numbers = [19, 5, 42, 2, 77]
 
 const sumTwo = (numbers) => {
-    var sortedNumbers = numbers.map( x => +x)
-        .sort(function(a, b){return a-b});
-
-    var sumOfTwo = sortedNumbers[0] + sortedNumbers[1]
-    console.log(sumOfTwo)
+    const [firstNumber, secondNumber] = numbers.sort((a, b) =>  a-b);
+    console.log(firstNumber + secondNumber)
 }
 
 sumTwo(numbers);
