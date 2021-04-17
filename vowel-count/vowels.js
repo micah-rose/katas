4e74ef32871f7bcf132b2aeacf9dd3c556785034
Micah Lund <micah.r.lund@gmail.com>
2021-04-17 13:48:45 -0600

    Completed challenge by looping through a string and performing an if check on the variables in that string and counting each variable by one.

--- vowel-count/vowels.js
+++ vowel-count/vowels.js
@@ -1,5 +1,17 @@
 const vowelCount = (str) => {
     var count = 0;
 
+    for (const v of str){
+        if (v === "a" || 
+            v === "e" || 
+            v === "i" || 
+            v === "o" || 
+            v === "u"){
+                count++
+        }
+    }
+
     console.log(count);
 }
+
+vowelCount("abracadabra");
