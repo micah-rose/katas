2de1ef47ee4de644f5d836d616f9874120d26fe5
Micah Lund <micah.r.lund@gmail.com>
2021-04-16 18:30:39 -0600

    Started logic to break down a given text to be one combined string with no spaces.

--- replace-alpha/replace.js
+++ replace-alpha/replace.js
@@ -1,3 +1,17 @@
+
+const alpha = [
+    "a", "b", "c", "d", "e",
+    "f", "g", "h", "i", "j", 
+    "k", "l", "m", "n", "o", 
+    "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z"
+]
+
 const replaceAlpha = (text) => {
-    //logic to replace letters with their position in the alphabet
+    var lowerText = text.toLowerCase().replace(/ /g, '');
+    console.log(lowerText)
 }
+
+const text = "The sunset sets at twelve o' clock."
+
+replaceAlpha(text);
