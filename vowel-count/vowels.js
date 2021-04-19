23bb01d599358bb2fd9c31e4053506f9f821ab16
Micah Lund <micah.r.lund@gmail.com>
2021-04-19 10:52:32 -0600

    Modified the vowelCount challenge to using split with reduce to return the count of vowels.

--- vowel-count/vowels.js
+++ vowel-count/vowels.js
@@ -1,12 +1,7 @@
 const vowelCount = (str) => {
-    let count = 0;
-
-    for (const v of str){
-        if (v.match(/[aeiou]/g)) {
-            count++
-        }
-    }
-
+const count = str.split('').reduce((acc, v) => {
+    return v.match(/[aeiou]/g) ? acc + 1 : acc
+}, 0)
     console.log(count);
 }
 
